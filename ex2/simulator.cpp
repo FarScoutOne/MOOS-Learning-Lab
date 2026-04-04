538e10ac91530f4776a4fd59fb78cc4da0470363
ryne <rsmith23@live.maryville.edu>
2026-04-04 16:36:19 -0400

    Fix CMakeLists.txt for CMake 3.10, pthread linking, and simulator Iterate

--- ex2/simulator.cpp
+++ ex2/simulator.cpp
@@ -38,7 +38,7 @@ which does the work of the application.
 **/
 bool CSimulator::Iterate()
 {
-    statis int k = 0;
+    static int k = 0;
 
     if (k++%10 == 0)
     {
@@ -47,14 +47,14 @@ bool CSimulator::Iterate()
         dfHeading += MOOSWhiteNoise(0.1);
 
         //publish the data (2nd param is a double so it will be forever double data...)
-        m_Comms.Notify("Heading".dfHeading.MOOSTime());
+        m_Comms.Notify("Heading", dfHeading, MOOSTime());
     }
     
     if (k%35 == 0)
         {
             static double dfVolts = 100;
             dfVolts -= fabs(MOOSWhiteNoise(0.1));
-            std::string sStatus = MOOSFormat(std::format("Status={},BatteryVoltage={:.2f},Bilge={}",
+            std::string sStatus = std::format("Status={},BatteryVoltage={:.2f},Bilge={}",
                         dfVolts > 50.0 ? "Good" : "Bad",
                         dfVolts,
                         k%100 > 50 ? "On" : "Off");
